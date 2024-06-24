f95f45b3314e61690d0fbcca775c8cc9bdefdc85
SatoshiFunayama <akchan.acts@gmail.com>
2024-06-24 20:14:22 +0900

    Refactored to conform to the changed specification of BaseLoader class

--- src/pyfroc/loaders/seg_nrrd.py
+++ src/pyfroc/loaders/seg_nrrd.py
@@ -29,7 +29,7 @@ class SlicerSegmentation:
 
 @dataclass
 class SegNRRD:
-    space_directions: np.ndarray  # (3x3): (voxel_dir, xyz)
+    space_directions: np.ndarray  # (3x3): (voxel_dir_xyz, xyz_spacing)
     segmentations: tuple[SlicerSegmentation]
     mask: np.ndarray
 
@@ -40,18 +40,17 @@ class SegNRRD:
 
 
 class SegNRRDLoader(BaseLoader):
-    @classmethod
-    def read_responses(cls, case_dir_path: str) -> list[Response]:
+    def read_responses(self, case_dir_path: str) -> list[Response]:
         series_responses = []
 
-        for segnrrd_path in cls._list_segnrrd_path(case_dir_path):
-            segnrrd = cls._read_segnrrd(segnrrd_path)
-            series_responses.extend(cls._segnrrd2responses(segnrrd))
+        for segnrrd_path in self.list_segnrrd_path(case_dir_path):
+            segnrrd = self.read_segnrrd(segnrrd_path)
+            series_responses.extend(self.segnrrd2responses(segnrrd))
 
         return series_responses
 
-    @classmethod
-    def _segnrrd2responses(cls, segnrrd: SegNRRD, mask_dtype=np.int8) -> list[Response]:
+    @staticmethod
+    def segnrrd2responses(segnrrd: SegNRRD, mask_dtype=np.uint8) -> list[Response]:
         responses: list[Response] = []
 
         for seg in segnrrd.segmentations:
@@ -80,14 +79,14 @@ class SegNRRDLoader(BaseLoader):
 
         return responses
 
-    @classmethod
-    def _mask2minsphere(cls, mask: np.ndarray,
+    @staticmethod
+    def mask2minisphere(mask: np.ndarray,
                         space_directions: np.ndarray,
-                        mask_dtype=np.int32) -> tuple[np.ndarray, float]:
+                        mask_dtype=np.int8) -> tuple[np.ndarray, float]:
         # Consider only the edge points to reduce the computational cost
         mask = (mask > 0).astype(mask_dtype)
 
-        assert mask.max() > 0, "mask should have at least one positive value"
+        assert mask.max() > 0, "mask should have at least one positive cell"
 
         mask_edge = mask - binary_erosion(mask).astype(mask_dtype)
 
@@ -105,22 +104,22 @@ class SegNRRDLoader(BaseLoader):
 
         return c, r
 
-    @classmethod
-    def _idx2coords(cls, idx, space_directions, origin=np.zeros(3)):
+    @staticmethod
+    def idx2coords(idx, space_directions, origin=np.zeros(3)):
         return np.dot(idx, space_directions) + origin
 
-    @classmethod
-    def _list_segnrrd_path(cls, dir_path) -> list[str]:
+    @staticmethod
+    def list_segnrrd_path(dir_path) -> list[str]:
         return glob.glob(os.path.join(dir_path, "*.seg.nrrd"))
 
-    @classmethod
-    def _read_segnrrd(cls, segnrrd_path) -> SegNRRD:
+    @staticmethod
+    def read_segnrrd(segnrrd_path) -> SegNRRD:
         vol, header = nrrd.read(segnrrd_path)
 
         if vol.ndim == 3:
             vol = np.expand_dims(vol, axis=0)
 
-        parsed_header = cls._parse_seg_nrrd_header(header)
+        parsed_header = SegNRRDLoader.parse_seg_nrrd_header(header)
 
         segnrrd = SegNRRD(
             space_directions=parsed_header["space_directions"],
@@ -130,8 +129,8 @@ class SegNRRDLoader(BaseLoader):
 
         return segnrrd
 
-    @classmethod
-    def _parse_confidence_from_seg_name(cls, name: str) -> int:
+    @staticmethod
+    def parse_confidence_from_seg_name(name: str) -> int:
         """Take the confidence value from the segmentation name.
         The first integer included in the name is considered as the confidence value.
 
@@ -146,8 +145,8 @@ class SegNRRDLoader(BaseLoader):
             return int(m.group(1))
         return -1
 
-    @classmethod
-    def _parse_seg_nrrd_header(cls, header: dict) -> dict:
+    @staticmethod
+    def parse_seg_nrrd_header(header: dict) -> dict:
         ret = {
             "space_directions": None,
             "n_layers": -1,
@@ -187,7 +186,7 @@ class SegNRRDLoader(BaseLoader):
             elif key.endswith("Name"):
                 name = header[key]
                 seg_dict[id].name = name
-                seg_dict[id].confidence = cls._parse_confidence_from_seg_name(name)
+                seg_dict[id].confidence = SegNRRDLoader.parse_confidence_from_seg_name(name)
 
         ret["segmentations"] = tuple(seg_dict.values())
 
