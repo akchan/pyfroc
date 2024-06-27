27a09b168b2fd8e1d0886915d5258cd4cdd92f35
SatoshiFunayama <akchan.acts@gmail.com>
2024-06-27 22:39:52 +0900

    Change coordinate sytem

--- src/pyfroc/loaders/seg_nrrd.py
+++ src/pyfroc/loaders/seg_nrrd.py
@@ -12,7 +12,7 @@ import numpy as np
 from skimage.measure import label
 from skimage.morphology import binary_erosion
 
-from pyfroc.coords import SeriesCoordinates
+from pyfroc.coords import ScannerCoordinates
 from pyfroc.loaders.base_loader import BaseLoader
 from pyfroc.signals import Response
 from pyfroc.miniball_util import get_min_sphere
@@ -30,12 +30,14 @@ class SlicerSegmentation:
 @dataclass
 class SegNRRD:
     space_directions: np.ndarray  # (3x3): (voxel_dir_xyz, xyz_spacing)
+    origin: np.ndarray
     segmentations: tuple[SlicerSegmentation]
     mask: np.ndarray
 
     # Check attributes
     def __post_init__(self):
         assert self.space_directions.shape == (3, 3), f"Invalid shape of space_directions: {self.space_directions.shape}"
+        assert self.origin.shape == (3,), f"Invalid shape of origin: {self.origin.shape}"
         assert isinstance(self.segmentations, tuple), "segmentation should be a tuple"
 
 
@@ -65,10 +67,12 @@ class SegNRRDLoader(BaseLoader):
             for label_i in range(1, label_max + 1):
                 mask_i = (mask_labeled == label_i).astype(mask_dtype)
 
-                c, r = SegNRRDLoader.mask2minisphere(mask_i, segnrrd.space_directions)
+                c, r = SegNRRDLoader.mask2minisphere(mask_i,
+                                                     segnrrd.space_directions,
+                                                     origin=segnrrd.origin)
                 assert r > 0.0, f"Invalid radius or {r} for {seg}"
 
-                res = Response(coords=SeriesCoordinates(*c),
+                res = Response(coords=ScannerCoordinates(*c),
                                r=r,
                                name=seg.name,
                                confidence=seg.confidence)
@@ -80,19 +84,18 @@ class SegNRRDLoader(BaseLoader):
     @staticmethod
     def mask2minisphere(mask: np.ndarray,
                         space_directions: np.ndarray,
+                        origin: np.ndarray = np.zeros(3),
                         mask_dtype=np.int8) -> tuple[np.ndarray, float]:
-        # Consider only the edge points to reduce the computational cost
         mask = (mask > 0).astype(mask_dtype)
 
         assert mask.max() > 0, "mask should have at least one positive cell"
 
-        mask_edge = mask - binary_erosion(mask).astype(mask_dtype)
+        # Convert idx to scanner coordinates
+        xx, yy, zz = np.where(mask > 0)
+        idx = np.array([xx, yy, zz], dtype=np.float32)
 
-        # Get series coordinates of the edge points
-        xx, yy, zz = np.where(mask_edge > 0)
-        edge_coords = xx[:, None] * space_directions[None, 0]\
-            + yy[:, None] * space_directions[None, 1]\
-            + zz[:, None] * space_directions[None, 2]
+        # (n_points, 3)
+        edge_coords = np.dot(space_directions.T, idx).T + origin
 
         if len(edge_coords) == 1:
             r = np.max(space_directions)
@@ -101,10 +104,6 @@ class SegNRRDLoader(BaseLoader):
         c, r = get_min_sphere(edge_coords)
 
         return c, r
-
-    @staticmethod
-    def idx2coords(idx, space_directions, origin=np.zeros(3)):
-        return np.dot(idx, space_directions) + origin
 
     @staticmethod
     def list_segnrrd_path(dir_path) -> list[str]:
@@ -121,6 +120,7 @@ class SegNRRDLoader(BaseLoader):
 
         segnrrd = SegNRRD(
             space_directions=parsed_header["space_directions"],
+            origin=parsed_header["origin"],
             segmentations=parsed_header["segmentations"],
             mask=vol
         )
@@ -147,26 +147,15 @@ class SegNRRDLoader(BaseLoader):
     def parse_seg_nrrd_header(header: dict) -> dict:
         ret = {
             "space_directions": None,
+            "origin": None,
             "n_layers": -1,
             "segmentations": [],
         }
 
         seg_dict = {}
 
+        # Segmentations
         for key in header.keys():
-            # voxel size
-            if key == "sizes":
-                ary = header[key]
-                if len(ary) == 3:
-                    ret["n_layers"] = 1
-                    ret["voxel_size"] = tuple(ary)
-                elif len(ary) == 4:
-                    ret["n_layers"] = ary[0]
-                    ret["voxel_size"] = tuple(ary[1:])
-                else:
-                    raise ValueError(f"Invalid len(sizes) = {len(ary)}")
-
-            # Segmentations
             m = re.match(r"Segment([0-9])+_.*", key)
 
             if m is None:
@@ -186,8 +175,21 @@ class SegNRRDLoader(BaseLoader):
                 seg_dict[id].name = name
                 seg_dict[id].confidence = SegNRRDLoader.parse_confidence_from_seg_name(name)
 
+        # voxel size
+        volume_shape = header["sizes"]
+        if len(volume_shape) == 3:
+            ret["n_layers"] = 1
+            ret["voxel_size"] = tuple(volume_shape)
+        elif len(volume_shape) == 4:
+            ret["n_layers"] = volume_shape[0]
+            ret["voxel_size"] = tuple(volume_shape[1:])
+        else:
+            raise ValueError(f"Invalid len(sizes) = {len(volume_shape)}")
+
         ret["segmentations"] = tuple(seg_dict.values())
 
         ret["space_directions"] = np.array(header["space directions"][-3:], dtype=np.float32)
+
+        ret["origin"] = np.array(header["space origin"][-3:], dtype=np.float32)
 
         return ret
