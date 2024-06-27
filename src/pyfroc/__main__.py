70516f46bb52344dbdce72a736146dcbcc942eb6
SatoshiFunayama <akchan.acts@gmail.com>
2024-06-27 17:00:06 +0900

    Update shell options

--- src/pyfroc/__main__.py
+++ src/pyfroc/__main__.py
@@ -4,11 +4,15 @@
 import argparse
 
 from pyfroc import prepare, evaluate
+from pyfroc.loaders import SegNRRDLoader
+from pyfroc.raters import NearestPairRater
+from pyfroc.writers import RJAFROCWriter
 
 
 def main():
     # Parsers for the main command and subcommands
-    parser = argparse.ArgumentParser(description="pyfroc: A Python package for FROC/JAFROC analysis.")
+    usage_text = "Usage: python -m pyfroc [command] [options]"
+    parser = argparse.ArgumentParser(description="pyfroc: A Python package for FROC/JAFROC analysis.",usage=usage_text, formatter_class=argparse.HelpFormatter)
     subparsers = parser.add_subparsers(dest='command')
 
     # 'prepare' subcommand parser
@@ -16,6 +20,8 @@ def main():
     parser_prepare.add_argument('--dicom-dir', type=str, required=True, help='Path to the root directory of DICOM files used in the experiment')
     parser_prepare.add_argument('--target-dir', type=str, required=True, help='Path to the target directory where the prepared files will be stored')
     parser_prepare.add_argument('--num-of-raters', type=int, default=3, help='Number of raters in the experiment. Default is 3.')
+    parser_prepare.add_argument('--num-of-modalities', type=int, default=2,
+                                help='Number of modalities (or treatments) in the experiment. Default is 2.')
 
     # 'evaluate' subcommand parser
     parser_eval = subparsers.add_parser('evaluate', help='Evaluate the responses of the raters and generate a xlsx file for the RJAFROC analysis.')
@@ -27,13 +33,32 @@ def main():
                              help='File type of the evaluation results.')
     parser_eval.add_argument('--criteria', choices=["within_lesion"], default="within_lesion",
                              help='Criteria for positive responses.')
+    parser_eval.add_argument('--out-format', choices=["rjafroc_xlsx"], default="rjafroc_xlsx",
+                             help='Output file format')
 
     args = parser.parse_args()
 
     if args.command == 'prepare':
-        prepare(args.dicom_dir, args.target_dir, args.num_of_raters)
+        print(args.dicom_dir)
+        prepare(args.dicom_dir, args.target_dir, args.num_of_raters, args.num_of_modalities)
+
     elif args.command == 'evaluate':
-        evaluate(args.out_path, args.filetype, args.criteria, args.out_path)
+        if args.filetype == "segnrrd":
+            loader_class = SegNRRDLoader
+        else:
+            raise ValueError(f"Unknown loader class flag: {args.filetype}")
+
+        if args.criteria == "WithinLesionRater":
+            rater_class = NearestPairRater
+        else:
+            raise ValueError(f"Unknown rater class flag: {args.criteria}")
+
+        if args.out_format == "rjafroc_xlsx":
+            writer_class = RJAFROCWriter
+        else:
+            raise ValueError(f"Unknown writer class flag: {args.out_format}")
+
+        evaluate(args.out_path, loader_class, rater_class, writer_class, args.out_path)
     else:
         parser.print_help()
 
